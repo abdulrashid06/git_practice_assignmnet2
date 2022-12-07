c3a7430ea18f110087219d1c300416b3933686ba
abdulrashid06 <abdulrashid987655@gmail.com>
2022-12-07 12:31:37 +0530

    Prime number code written

---
+++ prime_Problem.js
@@ -0,0 +1,14 @@
+
+let num=6;
+let factor=0;
+for (let i=1; i<num; i--){
+   if (num%j==0){
+     factor++;
+   }
+ }
+ if (factor==2){
+   console.log("Yes");
+ }
+ else{
+   console.log("No");
+ }
